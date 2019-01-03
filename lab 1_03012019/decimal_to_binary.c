fd83a002a9648398e190ea44a6e1ce8c908a2e9d
Liyakat Dafedar <17co23@aitdgoa.edu.in>
2019-01-03 10:08:03 +0530

    first push

---
+++ lab 1_03012019/decimal_to_binary.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int a[10], i;
+void decimal_to_binary(int);
+void binary_to_decimal();
+int main()
+{
+    int no, choice, decimal;
+
+    decimal = no;
+    while(1)
+    {
+        printf("\n1:Decimal to binary\n2:binary to decimal\n\tEnter your choice(1-2)");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1: printf("Enter the decimal number\t:");
+                    scanf("%d",&no);
+                    decimal_to_binary(no);
+                    break;
+            case 2: printf("Enter the binary number(enter 2 to stop):\n");
+                    binary_to_decimal();
+                    break;
+            case 3: exit(0);
+            default : printf("\nEnter valid option");
+                    break;;
+        }
+    }
+    return 0;
+}
+void decimal_to_binary(int n)
+{
+    int  j;
+    for(i=0;n > 0;i++,n=n/2)
+    {
+        a[i] = n %2;
+    }
+    a[i] = 2;
+
+    for(j=i-1;j>=0;j--)
+    {
+        printf("%d",a[j]);
+    }
+    printf("\n");
+}
+
+void binary_to_decimal()
+{
+    int j, no, n;
+    int k;
+    k = 0; no = 0;
+    printf("Enter the binary no:\t");
+    scanf("%d",&n);
+    while(n)
+    {
+        no += (n%10) + pow(2,i);
+        n = n / 10;
+        i++;
+    }
+
+
+
+    printf("\nThe equivalent decimal no = %d",no);
+
+}
+
+
+
+
+
+
+
