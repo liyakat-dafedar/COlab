cab817550232dbb92db04070450dd02cb9d7add7
liyakat <liyakatdafedar786#gmail.com>
2019-01-13 20:55:14 +0530

    modified code

--- lab 1_03012019/decimal_to_binary.c
+++ lab 1_03012019/decimal_to_binary.c
@@ -1,73 +1,74 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
-int a[10], i;
 void decimal_to_binary(int);
-void binary_to_decimal();
+void binary_to_decimal(int);
 int main()
 {
-    int no, choice, decimal;
-
-    decimal = no;
+    int no, choice, n;
     while(1)
     {
-        printf("\n1:Decimal to binary\n2:binary to decimal\n\tEnter your choice(1-2)");
+        printf("\n1:Decimal to binary\n2:binary to decimal\n3:Exit\n\tEnter your choice(1-3):\t");
         scanf("%d",&choice);
         switch(choice)
-        {
-            case 1: printf("Enter the decimal number\t:");
-                    scanf("%d",&no);
-                    decimal_to_binary(no);
-                    break;
-            case 2: printf("Enter the binary number(enter 2 to stop):\n");
-                    binary_to_decimal();
-                    break;
-            case 3: exit(0);
-            default : printf("\nEnter valid option");
-                    break;;
-        }
+        	{
+	            case 1 : printf("Enter the decimal number\t:");
+	                     scanf("%d",&no);
+	                     decimal_to_binary(no);
+	                     break;
+	            case 2 : printf("Enter the binary number(enter 2 to stop):\n");
+	                     printf("Enter the binary no:\t");
+	                     scanf("%d",&n);
+	                     binary_to_decimal(n);
+	                     break;
+	            case 3 : exit(0);
+	            default : printf("\nEnter valid option:\t");
+	                      break;
+        	}
     }
     return 0;
 }
 void decimal_to_binary(int n)
 {
-    int  j;
-    for(i=0;n > 0;i++,n=n/2)
+    int  j, i, a[10], rem, bin;
+    bin = 0; i=1;
+    if (n < 0)
+        n *= -1;
+    // n = (int)m
+    // m = m - n;
+    while( n > 0 )
     {
-        a[i] = n %2;
+    	rem = n % 2;
+    	bin = bin + (rem * i);
+        n = n / 2;
+    	i*=10;
     }
-    a[i] = 2;
-
-    for(j=i-1;j>=0;j--)
-    {
-        printf("%d",a[j]);
-    }
-    printf("\n");
+    printf("binary Equivalent = %d\n",bin);
+    // if (m != 0)
+    // {
+    //     int binf, j;
+    //     j = 1; binf = 0;
+    //     while( int(m) != 1 )
+    //     {
+    //         m = m * 2;
+    //         binf = binf + (j * int(m));
+    //         j = j * 10;
+    //     }
+    // }
 }
 
-void binary_to_decimal()
+void binary_to_decimal(int n)
 {
-    int j, no, n;
-    int k;
-    k = 0; no = 0;
-    printf("Enter the binary no:\t");
-    scanf("%d",&n);
-    while(n)
+    int no ,i , rem;
+    no = 0; i=0;
+    if (n < 0)
+        n *= -1;
+    while(n > 0)
     {
-        no += (n%10) + pow(2,i);
+        rem = n % 10;
+        no += (rem * pow(2,i));
         n = n / 10;
         i++;
     }
-
-
-
     printf("\nThe equivalent decimal no = %d",no);
-
 }
-
-
-
-
-
-
-
