d2314b946d0e86b4fffdd15f1a53845bcc5a0692
Liyakat Dafedar <17co23@aitdgoa.edu.in>
2019-01-24 10:45:10 +0530

    Program with hexadecimal and octal

--- lab2_16012019/decimal_binary_hexadecimal.c
+++ lab2_16012019/decimal_binary_hexadecimal.c
@@ -1,58 +1,90 @@
+/*Programmer : Liyakath Dafedar*/
+/*Program : Octal to binary and vise verse , Hexadecimal to binary and vise verse*/
+/*Date : 25 - 1 - 18*/
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-void decimal_to_hexadecimal(int);
-void binary_to_hexadecimal(char []);
-int hexadecimal_to_decimal(char []);
-void hexadecimal_to_binary(char []);
+void decimal_to_hexadecimal_octal(int);
+void binary_to_hexadecimal_octal(char []);
+int octal_hexadecimal_to_decimal(char []);
+void octal_hexadecimal_to_binary(char []);
 int binary_to_decimal(char []);
 int decimal_to_binary(char []);
 char hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+int base = 16;
 int main()
 {
     int no, choice;
-    char n[40];
+    char n[40], n1[100];
     while(1)
     {
-        printf("\n\n1:Decimal to HexaDecimal\n2:Binary to HexaDecimal\n3:HexaDecimal to Decimal\n4:HexaDecimal to Binary\n5:Exit\n\tEnter your choice(1-5):\t");
+        printf("\n\n1:Decimal to HexaDecimal\n2:Binary to HexaDecimal\n3:HexaDecimal to Decimal\n4:HexaDecimal to Binary");
+        printf("\n\n5:Decimal to Octal\n6:Binary to Octal\n7:Octal to Decimal\n8:Octal to Binary\n9:Exit\n\tEnter your choice(1-5):\t");
+
         scanf("%d",&choice);
+
         switch(choice)
         	{
 	            case 1 : printf("\nEnter the decimal number\t:");
 	                     scanf("%d",&no);
-	                     decimal_to_hexadecimal(no);
+	                     base = 16;
+	                     decimal_to_hexadecimal_octal(no);
 	                     break;
 	            case 2 : printf("\nEnter the binary no:\t");
-                         scanf("%s",&n);
+                         scanf("%s",n);
+                         base = 16;
 	                     printf("\n\tHexaDecimal Equivalent = ");
-	                     binary_to_hexadecimal(n);
+	                     binary_to_hexadecimal_octal(n);
 	                     break;
 	            case 3 : printf("\nEnter the HexaDecimal number\t:");
-	                     scanf("%s",&n);
-	                     printf("\n\tDecimal Equivalent = %d\n",hexadecimal_to_decimal(n));
+	                     scanf("%s",n);
+	                     base = 16;
+	                     printf("\n\tDecimal Equivalent = %d\n",octal_hexadecimal_to_decimal(n));
 	                     break;
 	            case 4 : printf("\nEnter the HexaDecimal no:\t");
-                         scanf("%s",&n);
-	                     hexadecimal_to_binary(n);
+                         scanf("%s",n1);
+                         base = 16;
+	                     octal_hexadecimal_to_binary(n1);
 	                     break;
-	            case 5 : exit(0);
+                case 5 : printf("\nEnter the decimal number\t:");
+	                     scanf("%d",&no);
+	                     base = 8;
+	                     decimal_to_hexadecimal_octal(no);
+	                     break;
+	            case 6 : printf("\nEnter the binary no:\t");
+                         scanf("%s",n);
+                         base = 8;
+	                     printf("\n\tOctal Equivalent = ");
+	                     binary_to_hexadecimal_octal(n);
+	                     break;
+	            case 7 : printf("\nEnter the Octal number\t:");
+	                     scanf("%s",n);
+	                     base = 8;
+	                     printf("\n\tDecimal Equivalent = %d\n",octal_hexadecimal_to_decimal(n));
+	                     break;
+	            case 8 : printf("\nEnter the Octal no:\t");
+                         scanf("%s",n1);
+                         base = 8;
+	                     octal_hexadecimal_to_binary(n1);
+	                     break;
+	            case 9 : exit(0);
 	            default : printf("\nEnter valid option:\t");
 	                      break;
         	}
     }
     return 0;
 }
-void decimal_to_hexadecimal(int n)
+void decimal_to_hexadecimal_octal(int n)
 {
     char ch[10], temp[10];
     int  i, j, rem, len;
     i = 0;
     while(n > 0)
     {
-        rem = n % 16;
+        rem = n % base;
         ch[i] = hex[rem];
-        n = n / 16;
+        n = n / base;
         i++;
     }
     ch[i] = '\0';
@@ -61,14 +93,21 @@ void decimal_to_hexadecimal(int n)
         temp[j] = ch[len - j - 1];
 
     temp[j] = '\0';
+    if (base == 16)
     printf("\n\tHexaDecimal Equivalent = %s",temp);
+    else
+    printf("\n\tOctal Equivalent = %s",temp);
 }
 
 int binary_to_decimal(char bin[])
 {
-    int j, no, i;
+    int j, no, i, k;
     no = 0;i=0;
-    for(j=3;j>=0;j--,i++)
+    if (base == 16)
+    k = 3;
+    else
+    k = 2;
+    for(j=k;j>=0;j--,i++)
     {
         no += ((bin[j]-'0') * pow(2,i));
     }
@@ -76,28 +115,39 @@ int binary_to_decimal(char bin[])
 }
 
 
-void binary_to_hexadecimal(char bin[])
+void binary_to_hexadecimal_octal(char bin[])
 {
-    int i,j, decimal, count, lenb, norm;
+    int i,j, decimal, count, lenb, norm, no;
     char temp[40], normb[40];
-    i = 0; count = 0; norm = 16;
+    i = 0; count = 0;
+    if (base == 16)
+    {
+        norm = 16;
+        no = 4;
+    }
+    else
+    {
+        norm = 15;
+        no = 3;
+    }
     lenb = strlen(bin);
     // can normalize only 16 bits
-    norm = lenb + (4 - (lenb % 4));
+
+    norm = lenb + (no - (lenb % no));
     for (j = 0; j < norm - lenb; ++j)
     {
     	normb[j] = '0';
     }
     normb[j] = '\0';
     strcat(normb,bin);
-    printf("#-%s-#\n",normb );
+    //printf("#-%s-#\n",normb );
     for (i = 0; i < norm;i++)
     {
     	temp[count] = normb[i];
     	++count;
-    	if (count % 4 == 0)
+    	if (count % no == 0)
     	{
-    		temp[4] = '\0';
+    		temp[no] = '\0';
     		decimal = binary_to_decimal(temp);
     		printf("%c ",hex[decimal] );
     		count = 0;
@@ -106,7 +156,7 @@ void binary_to_hexadecimal(char bin[])
 
 }
 
-int hexadecimal_to_decimal(char dec[])
+int octal_hexadecimal_to_decimal(char dec[])
 {
 	int j, no, i, n[40], len;
     no = 0; i = 0; len = strlen(dec);
@@ -132,22 +182,24 @@ int hexadecimal_to_decimal(char dec[])
     i = 0;
     for(j=len-1;j>=0;j--,i++)
     {
-        no += ( n[j]* pow(16,i));
+        no += ( n[j]* pow(base,i));
     }
     //printf("Decimal Equivalent = %d\n",no );
     return no;
 }
 
 
-void hexadecimal_to_binary(char hexadecimal[])
+void octal_hexadecimal_to_binary(char hexadecimal[])
 {
-	int n, bin, rem, dec;
-	n = hexadecimal_to_decimal(hexadecimal);
+	long unsigned int n, bin, rem, dec;
+	n = octal_hexadecimal_to_decimal(hexadecimal);
+	printf("-%d-",n);
 	bin = 0; dec = 1;
     while(n > 0)
 	{
 		rem = n % 2;
 		bin = bin + (rem * dec);
+		//printf("--%ul--",bin);
 		dec *= 10;
 		n /=2;
 	}
