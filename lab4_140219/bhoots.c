9df82ebd50a192b2ce0a1f704d122abd819ca163
liyakat <liyakatdafedar786#gmail.com>
2019-03-07 07:44:04 +0530

    pg4 modified

--- lab4_140219/bhoots.c
+++ lab4_140219/bhoots.c
@@ -1,27 +1,41 @@
 #include<stdio.h>
 #include<string.h>
-
+void add(char[], char[]);
 void decimal_to_binary(int, char[]);
 void bhootsAlgorithm(char[], char[]);
-void two_s_complement(char bin1[],char bin2[],int operation)
+void two_s_complement(char[]);
+void sub(char[],char[]);
 char shift(char[]);
+char twoshift(char[],char[]);
 int main()
 {
-    int n1=12, n2=13;
-    char bin1[20], bin2[20], q;
+    int n1=12, n2=13,i;
+    char bin1[10], bin2[10], q;
+    char bin3[4];
+    for(i=0;i<4;i++)
+        bin3[i]='0';
     /*printf("Enter the first number:");
     scanf("%d",&n1);*/
-    decimal_to_binary(15,bin1);
+    decimal_to_binary(9,bin1);
     /*printf("Enter the second number:");
     scanf("%d",&n2);*/
     decimal_to_binary(9,bin2);
-
-    printf("binary Equivalent1 = %s\n",bin1);
+    //sub(bin1,bin2);
+    /*printf("binary Equivalent1 = %s\n",bin1);
     printf("binary Equivalent2 = %s\n",bin2);
+
+    //add(bin1,bin2);
     q = shift(bin1);
     printf("Q-1=%c\n",q);
     q = shift(bin2);
-    printf("Q-1=%c",q);
+    printf("Q-2=%c",q);  two_s_complement(bin2);*/
+
+    printf("binary Equivalent(M) = %s\n",bin1);
+    printf("binary Equivalent(Q) = %s\n",bin2);
+    //sub(bin3,bin2);
+//printf("binary Equivalent3 = %s--\n",bin3);
+    bhootsAlgorithm(bin1,bin2);
+
 }
 
 void decimal_to_binary(int n, char bin[])
@@ -41,36 +55,55 @@ void decimal_to_binary(int n, char bin[])
     }
     btemp[i] = '\0';
     len = strlen(btemp);
+
     for(i=len-1,j=0;i>=0;i--,j++)
         bin[j] = btemp[i];
-
+    if (strlen(bin) > len)
+        bin[len] = '\0';
 }
 
 void bhootsAlgorithm(char M[], char Q[])
 {
-    char A[20], Qminus1, Q0, len;
-    int count, i;
-    count = strlen(M);   Qminus1 = '0';  Q0 = Q[count - 1];
+
+    char A[5], Qminus1, Q0, Q_[2];
+    int count,len, i,k;
+    count = strlen(Q);   Qminus1 = '0';  Q0 = Q[count - 1];
     len = count - 1;
-    for(i=0;i<strlen(A);i++)
+    for(i=0;i<count;i++)
         A[i] = '0';
+
+    A[i] = '\0';
+    printf("A = %s\tQ = %s\tQ-1=%c\tM= %s\n\n",A,Q,Qminus1,M);
     for(i=1;i<=count;i++)
     {
         if (Q0 == '1' && Qminus1 == '0')
         {
             sub(A,M);
-            Qminus1 = shift(Q);
+            printf("[1,0]:A = %s\tQ = %s\tQ-1=%c\tM= %s\n",A,Q,Qminus1,M);
+            Qminus1 = twoshift(A,Q);
+            printf("[1,0]:A = %s\tQ = %s\tQ-1=%c\tM= %s\n\n",A,Q,Qminus1,M);
+            Q0 = Q[3];
         }
         else if (Q0 == '0' && Qminus1 == '1')
         {
-            sub(A,M);
-            Qminus1 = shift(Q);
+            add(A,M);
+            printf("\nSum:%s\n",A);
+            printf("[0,1]:A = %s\tQ = %s\tQ-1=%c\tM= %s\n",A,Q,Qminus1,M);
+            Qminus1 = twoshift(A,Q);
+            printf("[0,1]:A = %s\tQ = %s\tQ-1=%c\tM= %s\n\n",A,Q,Qminus1,M);
+            Q0 = Q[3];
         }
         else if (Q0 ==  Qminus1)
         {
-            Qminus1 = shift(Q);
+            printf("[ , ]:A = %s\tQ = %s\tQ-1=%c\tM= %s\n",A,Q,Qminus1,M);
+            Qminus1 = twoshift(A,Q);
+            printf("[ , ]A = %s\tQ = %s\tQ-1=%c\tM= %s\n\n",A,Q,Qminus1,M);
+            Q0 = Q[3];
         }
     }
+    A[i] = '\0';    M[i] = '\0';
+    //printf("binary Equivalent1 f= %s\n",A);
+    //printf("binary Equivalent2 f= %s\n",M);
 }
 
 char shift(char arr[])
@@ -79,48 +112,50 @@ char shift(char arr[])
     char atemp[20], Qm;
     for(i=0;i<strlen(arr);i++)
         atemp[i] = arr[i];
-    atemp[i] = '\0';    Qm = arr[(strlen(arr) - 1)];
+    Qm = arr[(strlen(arr)-1)];
+    atemp[i] = '\0';
     for(i=0;i<strlen(arr)-1;i++)
         arr[i+1] = atemp[i];
-    printf("%s--Qm=%c\n",arr,Qm);
     return(Qm);
 }
 
-void two_s_complement(char bin4[],char bin5[])
+void two_s_complement(char bin[])
 {
-    int n1, n2, i, len, f, pos, carry, n;
-    f = 0;  carry = 0;
-    char bin1[20], bin2[20];
-    for(i=0;i<strlen(bin4);i++)
-    {
-        bin1[i] = bin4[i];
-        bin2[i] = bin4[i];
-    }
+    int i, len, f, pos;
+    f = 0;
+    char bin2[20];
+    for(i=0;i<strlen(bin);i++)
+        bin2[i] = bin[i];
 
-        len = strlen(bin2);
-        for(i=len-1;i>=0;i--)
+    len = strlen(bin2);
+    for(i=len-1;i>=0;i--)
+    {
+        if (bin2[i] == '1')
         {
-            if (bin2[i] == '1')
-            {
-                f = 1;
-                pos = i;
-                break;
-            }
+            f = 1;
+            pos = i;
+            break;
         }
+    }
     for(i=pos-1;i>=0;i--)
     {
         if (bin2[i] == '0')
             bin2[i] = '1';
         else
             bin2[i] = '0';
-    }
-    add(bin1,bin2);
+    }
+    for(i=0;i<strlen(bin);i++)
+    {
+        bin[i] = bin2[i];
+    }
+    //add(bin2,bin2);
+    //printf("---%s---",bin );
 }
 void add(char bin1[], char bin2[])
 {
     char bin3[50];    int len,i,carry;
     len = strlen(bin1);
-    for(i=len-1;i>=0;i--)
+    for(i=3;i>=0;i--)
     {
         if (bin1[i] == '1' && bin2[i] == '1')
         {
@@ -166,15 +201,35 @@ void add(char bin1[], char bin2[])
     bin3[len] = '\0';
     for(i=0;i<strlen(bin1);i++)
         bin1[i] = bin3[i];
-      // printf("\nbin3:%s",bin3);
+ //      printf("\nbin3:%s",bin1);
 }
 
 
-
-
-
-
-
-
-
-
+void sub(char bin1[],char bin2[])
+{
+    char b3[5];
+    int i;
+    for(i=0;i<5;i++)
+        b3[i] = bin2[i];
+    b3[i] = '\0';
+    two_s_complement(b3);
+    add(bin1,b3);
+    //printf("bin1 = %s\n",bin1);
+}
+char twoshift(char a[],char b[])
+{
+    int i, len;
+    char qm, c[4], d[4];
+    len = strlen(a);
+    qm = a[len-1];
+    for (i = 0; i < 3; ++i)
+       { c[i] = a[i];   d[i] = b[i];   }
+    for (i = 0; i < 3; ++i)
+        a[i+1] = c[i];
+    b[0] = qm;
+    qm = b[len-1];
+    for (i = 0; i < 3; ++i)
+        b[i+1] = d[i];
+    //printf("\nA=%s,B=%s\n",a,b );
+    return qm;
+}
